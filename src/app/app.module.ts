d3439f046ae6a642850873492262066ece2639f1
Gregory King <gregory.king@factset.com>
2017-12-01 16:30:55 -0600

    added initial plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { SpdrApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -10,10 +10,14 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AppVersion } from '@ionic-native/app-version';
+import { TapticEngine } from '@ionic-native/taptic-engine';
+import { Network } from '@ionic-native/network';
+import { ThreeDeeTouch } from '@ionic-native/three-dee-touch';
 
 @NgModule({
   declarations: [
-    MyApp,
+    SpdrApp,
     AboutPage,
     ContactPage,
     HomePage,
@@ -21,11 +25,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(SpdrApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    SpdrApp,
     AboutPage,
     ContactPage,
     HomePage,
@@ -34,6 +38,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    AppVersion,
+    TapticEngine,
+    Network,
+    ThreeDeeTouch,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
