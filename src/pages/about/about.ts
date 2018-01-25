2cc7db524c24fd16809178977749ed96c2b870ef
Gregory King <gregory.king@factset.com>
2018-01-25 13:30:32 -0600

    added version info to the about tab

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { NativeStorage } from '@ionic-native/native-storage';
+import { AppVersion } from '@ionic-native/app-version';
 
 @Component({
   selector: 'page-about',
@@ -9,9 +10,11 @@ import { NativeStorage } from '@ionic-native/native-storage';
 })
 export class AboutPage {
   pushToken: string;
+  version: string;
 
   constructor(public navCtrl: NavController,
-    private nativeStorage: NativeStorage) {
+    private nativeStorage: NativeStorage,
+    private appVersion: AppVersion) {
 
   }
 
@@ -19,8 +22,14 @@ export class AboutPage {
     // TODO: this should be in the alerts provider
     this.nativeStorage.getItem('pushToken')
       .then(
-      data => this.pushToken = data.registrationId,
-      error => console.error(error)
+        data => this.pushToken = data.registrationId,
+        error => console.error(error)
+      );
+
+    this.appVersion.getVersionNumber()
+      .then(
+        version => this.version = version
       );
   }
+
 }
