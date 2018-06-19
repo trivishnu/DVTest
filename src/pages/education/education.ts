82344c7acfe7450d98fb0cc78ad6a0ca6337b7ee
Gregory King <gregory.king@factset.com>
2018-06-19 16:32:38 -0500

    removed cruft from earlier attempt

--- src/pages/education/education.ts
+++ src/pages/education/education.ts
@@ -9,27 +9,8 @@ import { DetailPage } from '../detail/detail';
   templateUrl: 'education.html',
 })
 export class EducationPage {
-  items: any = [];
-  itemExpandHeight: number = 100;
 
   constructor() {
-    this.items = [
-      { expanded: false },
-      { expanded: false },
-      { expanded: false }
-    ];
-  }
-
-  expandItem(item) {
-    this.items.map((listItem) => {
-      if (item == listItem) {
-        listItem.expanded = !listItem.expanded;
-      } else {
-        listItem.expanded = false;
-      }
-
-      return listItem;
-    });
   }
 
 }
