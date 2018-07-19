906d3bc45dc308cfeb743001531cc47354548b3d
Damini <damini.damini@factset.com>
2018-07-19 18:20:12 +0530

    Changes made for how to purchase page

--- src/components/fund-details/fund-details.ts
+++ src/components/fund-details/fund-details.ts
@@ -60,7 +60,12 @@ export class FundDetailsComponent {
   }
 
   howToPurchase() {
-    this.navCtrl.push(HowToPurchasePage);
+    this.navCtrl.push(HowToPurchasePage,
+    {
+      sectorsymbol: this.symbol,
+      sectorname:this.sectorName
+    });
+    
   }
 
 }
--- src/pages/how-to-purchase/how-to-purchase.ts
+++ src/pages/how-to-purchase/how-to-purchase.ts
@@ -10,28 +10,31 @@ import { SectorSpdrService } from '../../providers/SectorSpdrAPI';
 })
 export class HowToPurchasePage {
 
-  @Input() symbol: string;
+  symbol: string;
   sectorName: string;
   generalDisclaimer: string;
-
+  changedDisclaimer:any;
+  str:string;
+  
   constructor(private sectorSpdrService: SectorSpdrService, public navCtrl: NavController, public navParams: NavParams) {
+    this.symbol=navParams.get("sectorsymbol");
+    this.sectorName=navParams.get("sectorname");
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HowToPurchasePage');
+    console.log(this.sectorName);
+    console.log(this.symbol);
+    this.changedDisclaimer= document.getElementsByClassName("disclaimer")[0].innerHTML;
+    this.str=this.changedDisclaimer.replace('{sectorname}',this.sectorName);
 
   }
 
   ngOnInit() {
-    var sector = this.sectorSpdrService.getSectorInfo(this.symbol);
-    if (sector !== undefined) {
-      this.sectorName = sector.sectorName;
-    }
-
-    this.sectorSpdrService.getDisclaimerContent('How To Purchase (Mobile)')
+      this.sectorSpdrService.getDisclaimerContent('How To Purchase (Mobile)')
       .subscribe(resp => {
         this.generalDisclaimer = resp;
-      });
+        });
 
   }
 
